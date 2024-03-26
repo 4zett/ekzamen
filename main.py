2faa187dc6142c43464aa899edcac2deeea38f6a
4zett <99506244+4zett@users.noreply.github.com>
2024-03-26 16:33:32 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import re
+
+def _regular(filename):
+    with open(filename, 'r') as file:
+        data = file.read()
+
+    pattern = r'\b2\d{4}\b'
+    matches = re.findall(pattern, data)
+
+    numbers = [int(num) for num in matches if int(num) % 2 == 0 and int(num) > 0]
+
+    _sum = sum(numbers)
+    count = len(numbers)
+    max_number = max(numbers) if numbers else None
+
+    return _sum, count, max_number
+
+def main():
+    filename = 'ekzamen.txt'
+    _sum, count, max_number = _regular(filename)
+
+    print("Сумма натуральных четных пятиразрядных чисел с первой цифрой 2:", _sum)
+    print("Колво найденных чисел:", count)
+    if max_number is not None:
+        print("Макс число:", max_number)
+    else:
+        print("Ни одного подходящего числа не найдено.")
+
+if __name__ == "__main__":
+    main()
